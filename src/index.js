524b1dd85aa8fe0976f825d9dfeb8031374483d7
yunusyerli1 <yunus.yerli@gaboras.com>
2022-01-17 15:18:31 +0300

    jest installed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-const express = require('express')
-require('./db/mongoose');
-const { ObjectId } = require('mongodb');
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const app = require('./app');
 
-const app = express();
 const port = process.env.PORT;
-
-
-
-//Post işleminde gönderdiğimiz params json olarak kullanılmasını sağlar
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
 
 app.listen(port, () => {
     console.log('Server is up on port', +port);
